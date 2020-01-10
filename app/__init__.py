675cbc245eb7884cb28de0bb7410da78288a9696
Patone <I694442@NAEAST.AD.JPMORGANCHASE.com>
2020-01-10 14:30:09 -0500

    added babel

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_babel import Babel
 import logging
 from logging.handlers import SMTPHandler
 from logging.handlers import RotatingFileHandler
 import os
-from flask_moment import Moment
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -20,9 +22,11 @@ login.login_view = 'login'
 bootstrap = Bootstrap(app)
 mail = Mail(app)
 moment = Moment(app)
+babel = Babel(app)
 
 
 from app import routes, models, errors
+
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -52,3 +56,7 @@ if not app.debug:
         app.logger.setLevel(logging.INFO)
         app.logger.info('Microblog startup')
 
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
--- app/email.py
+++ app/email.py
@@ -1,7 +1,7 @@
-# from threading import Thread
-# from flask import render_template
-# from flask_mail import Message
-# from app import app, mail
+from threading import Thread
+from flask import render_template
+from flask_mail import Message
+from app import app, mail
 
 
 def send_async_email(app, msg):
