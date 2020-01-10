77b9f4c570038c39fceb7ced3d89422df57c0be2
Patone <I694442@NAEAST.AD.JPMORGANCHASE.com>
2020-01-10 12:45:40 -0500

    added flask mail

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,13 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-# from flask_mail import Mail
+from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 import logging
 from logging.handlers import SMTPHandler
 from logging.handlers import RotatingFileHandler
 import os
-
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -17,8 +17,10 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
-# mail = Mail(app)
 bootstrap = Bootstrap(app)
+mail = Mail(app)
+moment = Moment(app)
+
 
 from app import routes, models, errors
 
